03c1c5fe13ceb049aebce6e1db50812a0f89ec1d
aamilnasar <aamilaroopa@gmail.com>
2023-06-29 19:33:54 +0530

    hi

---
+++ Python/class_1.py
@@ -0,0 +1,9 @@
+print("Hey!, you know i am studying python") #This is used to print the string
+a= 12
+b=14
+print("Hi my is age "+str(b)+" The second value "+str(a))
+ 
+#This is the second section of using input
+
+cd=input("Hey!, user enter your age man : ")
+print("Hey!, Your so young your age is "+str(cd)+" unbelavabile age")
