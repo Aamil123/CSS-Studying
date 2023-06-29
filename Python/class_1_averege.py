df7b0121c90b46cb6a47c9b43c7ec3720608a073
aamilnasar <aamilaroopa@gmail.com>
2023-06-29 19:58:07 +0530

    i do the first project

---
+++ Python/class_1_averege.py
@@ -0,0 +1,10 @@
+num1=int(input("PLS Enter 3 number : "))
+num2=int(input())
+num3=int(input())
+
+sum=num1+num2+num3
+
+averege=(num1+num2+num3)/3
+
+print("The averege is :"+str(averege))
+print("The sum is :"+str(sum))
