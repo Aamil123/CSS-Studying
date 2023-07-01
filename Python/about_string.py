6789092febcbbf840d13cf74a7fa014ca9c8f1c6
aamilnasar <aamilaroopa@gmail.com>
2023-07-01 22:43:14 +0530

    hi

---
+++ Python/about_string.py
@@ -0,0 +1,12 @@
+a="Hi, my name is Aamil Nasar i am very handsome you know"
+
+print(len(a))#this is used to count the characters in the string
+
+
+#Now i want to print only 10th character
+#Then i can do this
+print(a[10])
+
+#Now i want to print only 0 character to 20th charecter
+#Then i can do this
+print(a[0:20])
