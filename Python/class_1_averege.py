f81131a6c8980faf991ca990a2ccc83e50f50de0
aamilnasar <aamilaroopa@gmail.com>
2023-07-01 22:31:56 +0530

    some changes i do

--- Python/class_1_averege.py
+++ Python/class_1_averege.py
@@ -8,3 +8,8 @@ averege=(num1+num2+num3)/3
 
 print("The averege is :"+str(averege))
 print("The sum is :"+str(sum))
+
+# how can i identify the type of the value entered by user
+x=float(input("Enter a number pls :"))
+
+print("The type of the number you enterd is "+str(type(x)))
