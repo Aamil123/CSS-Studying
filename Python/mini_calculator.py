366cef626777e4422f68330a051ff21499ea73af
aamilnasar <aamilaroopa@gmail.com>
2023-07-02 10:06:51 +0530

    i created  a mini calculator using python

---
+++ Python/mini_calculator.py
@@ -0,0 +1,16 @@
+#I am building a mini calculator
+
+a=int(input("Pls enter 2 numbers :"))
+b=int(input())
+
+sum=a+b
+diffrence=a-b
+divition=a/b
+multiplication=a*b
+averege=(a+b)/3
+
+print("Sum = "+str(sum))
+print("Diffrence = "+str(diffrence))
+print("Divition = "+str(divition))
+print("Multiplication = "+str(multiplication))
+print("Averege = "+str(averege))
