3e358de12bce755b8e125949c381143def732a9e
aamilnasar <aamilaroopa@gmail.com>
2023-07-02 12:27:22 +0530

    class 2 lists added

--- Python/class_2_lists.py
+++ Python/class_2_lists.py
@@ -1,2 +1,9 @@
 a=["Aamil","Nihal","Nasar","Aroopa","Python"]
 print(a)
+
+b=input('Enter the any name you wanted to add to Lists :')
+
+a.append(b)
+
+print(a)
+
--- Python/mini_calculator.py
+++ Python/mini_calculator.py
@@ -10,7 +10,12 @@ multiplication=a*b
 averege=(a+b)/3
 
 print("Sum = "+str(sum))
+print("*******************************")
 print("Diffrence = "+str(diffrence))
+print("*******************************")
 print("Divition = "+str(divition))
+print("*******************************")
 print("Multiplication = "+str(multiplication))
+print("*******************************")
 print("Averege = "+str(averege))
+print("*******************************")
