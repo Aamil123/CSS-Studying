dd51b491f9168dee850eefbe9f2342840b02a2eb
aamilnasar <aamilaroopa@gmail.com>
2023-07-02 15:07:31 +0530

    a project based on control statement is done

---
+++ Python/project_based_on_control_statements.py
@@ -0,0 +1,11 @@
+num1=int(input("Enter 2 numbers : "))
+num2=int(input())
+
+if  num1<num2:
+    print("The "+str(num2)+" is greater than "+str(num1))
+    
+elif num1==num2:
+    print("The two numbers is equal")
+    
+else:
+    print("The "+str(num1)+" is greater than "+str(num2))
