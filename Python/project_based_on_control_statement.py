779f4b383772b0b36f41a971e03ce6a55bf77768
aamilnasar <aamilaroopa@gmail.com>
2023-07-05 22:45:41 +0530

    an menu project is complted

--- Python/project_based_on_control_statement.py
+++ Python/project_based_on_control_statement.py
@@ -1,5 +1,21 @@
 menu='\n1.Protta\n2.Biriyani\n3.Mandhi\n4.Beef Roast\n5.Alfam\n'
+userInput=input("Pls enter what dish you want"+"\n"+menu)
 
-match menu:
-    case "1.Protta":
-        print("mhr")
+if "Protta" in userInput:
+    print("You selected " +userInput)
+
+elif "Biriyani" in userInput:
+    print("You selected "+userInput)
+    
+elif "Mandhi" in userInput:
+    print("You selected "+userInput)
+    
+elif "Beef Roast" in userInput:
+    print("You selected "+userInput)
+    
+elif "Alfam" in userInput:
+    print("You selected "+userInput)
+
+else:
+    print("\nSorry this dish is not available on this Aamil 's Hotel")
+    input("Any else sir : ")
