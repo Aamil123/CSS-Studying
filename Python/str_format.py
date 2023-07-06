6d34136c91263bf1a051d562a045d9b6154285ba
aamilnasar <aamilaroopa@gmail.com>
2023-07-06 22:17:09 +0530

    as

--- Python/str_format.py
+++ Python/str_format.py
@@ -5,3 +5,19 @@ print('{0} was {1} years old when he wrote this book'.format(name, age)) #the nu
 print('Why is {0} playing with that python?'.format(name))
 
 print("\n",name + ' is ' + str(age) + ' years old')
+
+
+#We can also name the parameters
+print('{name} was {age} years old when he wrote this book'.format(name=name, age=age))
+print('Why is {name} playing with that python?'.format(name=name))
+
+#Python 3.6 introduced a shorter way to do named parameters, called "f-strings"
+print(f'{name} was {age} years old when he wrote this book')  # notice the 'f' before the string
+print(f'Why is {name} playing with that python?')  # notice the 'f' before the string
+
+print('\n\n{0:_^11}'.format('hello'))
+
+#you can end with a space:
+print('a', end=' ')
+print('b', end=' ')
+print('c')
