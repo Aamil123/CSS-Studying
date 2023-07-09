0902dcbd88cae7be184730c65c85cfd5ed0c20fb
Aamil <90494101+Aamil123@users.noreply.github.com>
2023-07-09 10:20:46 +0530

    Update about_string.py

--- Python/about_string.py
+++ Python/about_string.py
@@ -6,6 +6,7 @@ print(len(a))#this is used to count the characters in the string
 #Now i want to print only 10th character
 #Then i can do this
 print(a[10])
+#hoooooooooooooooooo
 
 #Now i want to print only 0 character to 20th charecter
 #Then i can do this
