024556dae22ab3a12273e1723a1180d679d05263
aamilnasar <aamilaroopa@gmail.com>
2023-07-09 12:25:45 +0530

    str methods is added

---
+++ Python/bro_code/string_methods.py
@@ -0,0 +1,12 @@
+name = "Aaalmil"
+
+# print(len(name)) Get the lenght of the name 
+# print(name.find('a')) Using tthis method we can find were is the charecter
+# print(name.capitalize()) Only capitalize the first letter
+# print(name.upper()) This makes the string upper case
+# print(name.lower()) This make the string lower case
+# print(name.isdigit()) to check if the passed character is a digit
+# print(name.isalpha()) It is used to check if the passed character is alphabetic. or not.
+# print(name.count('a')) Returns the number of times an object appears in a list.
+# print(name.replace("l","N")) replaces a specified phrase with another specified phrase
+# print(name*3)
